eb6b15af8f7ff90ed9b48834593fce061848848b
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-11-26 15:13:03 -0700

    Add project files.

---
+++ Assignment3/Utility/Node Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Node
+    {
+        public string Value { get; set; } // data part
+        public Node Next { get; set; } // link or connection part
+
+        public Node(string value) //constructor of MyNode
+        {
+            Value = value;
+            Next = null;
+        }
+
+    }
+}
---
+++ Assignment3/Utility/SSL Class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class SSL : ILinkedListADT
+    {
+        //o Prepend an item to the beginning of the linked list.
+
+
+        //o Append an item to the end of the linked list.
+
+
+        //o Remove an item at an index in the linked list.
+
+
+        //o Remove an item from the start of the linked list.
+
+
+        //o Remove an item from the end of the linked list.
+
+
+        //o Insert an item at a specific index in the linked list.
+
+
+        //o Replace an item in the linked list.
+
+
+        //o Get an item at an index in the linked list.
+
+
+        //o Get the index of an item in the linked list.
+
+
+        //o Check if the linked list has an item.
+
+
+        //o Clear all items in the linked list.
+
+
+        //o Get the number of items in the linked list.
+
+    }
+}
