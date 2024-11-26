d70d4366804f9bf400e05565cca9eeb6a076e6a1
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-11-26 15:20:58 -0700

    Created a SSL class and Node class inside the utility folder. Also added comments of each functionality that needs to be implemented.
    Then created a LinkedListTest class inside the Assignment3.Test project. Also added comments of unit test cases that needs to be implemented.

---
+++ Assignment3.Tests/LinkedListTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Tests
+{
+    public class LinkedListTest
+    {
+        //•	The linked list is empty.
+
+
+        //•	An item is prepended.
+
+
+        //•	An item is appended.
+
+
+        //•	An item is inserted at index.
+
+
+        //•	An item is replaced.
+
+
+        //•	An item is deleted from beginning of list.
+
+
+        //•	An item is deleted from end of list.
+
+
+        //•	An item is deleted from middle of list.
+
+
+        //•	An existing item is found and retrieved.
+
+
+        //•	The additional functionality is working.
+    }
+}
